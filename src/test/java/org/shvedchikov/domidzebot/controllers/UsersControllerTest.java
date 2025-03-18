ea2398d086ce268bb98e18595e43f120b35a9b4f
snswd <sanswed@gmail.com>
2025-03-19 02:40:32 +0300

    *fix bugs at props

--- src/test/java/org/shvedchikov/domidzebot/controllers/UsersControllerTest.java
+++ src/test/java/org/shvedchikov/domidzebot/controllers/UsersControllerTest.java
@@ -36,7 +36,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource(locations = "file://DomidzeBot/src/main/resources/application.properties")
+@TestPropertySource(locations = "file://src/main/resources/application.properties")
 public class UsersControllerTest {
     @Autowired
     private MockMvc mockMvc;
