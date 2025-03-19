75153f9b09fced0cd0a616fa1e07cf70e1212ddd
Alexander Shvedchikov <105097978+ConstableFraser@users.noreply.github.com>
2025-03-19 18:48:28 +0300

    Update CredentialsControllerTest.java

--- src/test/java/org/shvedchikov/domidzebot/controllers/CredentialsControllerTest.java
+++ src/test/java/org/shvedchikov/domidzebot/controllers/CredentialsControllerTest.java
@@ -35,7 +35,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestPropertySource(
-    properties = "appproperties.hash=djM7MT82MzYxgkA9iYtLdzeFSESCpzCuOptoS0mLMjKRT1dLRTEwUDBINk5rSzVFQDVFP4kzNjE9OTM2MTQzlUg2M001RT42SFE=")
+    properties = "appproperties.hash=djM7MT82MzYxgkA9iYtLdzeFSESCpzCuOptoS0mLM"
+    + "jKRT1dLRTEwUDBINk5rSzVFQDVFP4kzNjE9OTM2MTQzlUg2M001RT42SFE=")
 public class CredentialsControllerTest {
     private static SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor token;
     private static Credential testCredential;
