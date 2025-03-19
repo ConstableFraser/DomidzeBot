2f5d2d4e94117f316e97f0b22ad961f73b7fa78a
snswd <sanswed@gmail.com>
2025-03-19 20:28:43 +0300

    *fix bugs at props

--- src/main/java/org/shvedchikov/domidzebot/config/AppProperties.java
+++ src/main/java/org/shvedchikov/domidzebot/config/AppProperties.java
@@ -1,14 +1,16 @@
 package org.shvedchikov.domidzebot.config;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+// import org.springframework.context.annotation.PropertySource;
 
 @Data
 @Configuration
-@PropertySource("classpath:application.properties")
+// @PropertySource("classpath:application.properties")
 public class AppProperties {
-    @Value(value = "${appproperties.hash}")
-    private String hash;
+//    @Value(value = "${appproperties.hash}")
+    private final String hash = """
+        djM7MT82MzYxgkA9iYtLdzeFSESCpzCuOptoS0mLMjKRT1dLRTE\
+        wUDBINk5rSzVFQDVFP4kzNjE9OTM2MTQzlUg2M001RT42SFE=""";
 }
