6d57836e6f3ccb4df06ef30c9c331a83a53f233b
Alexander Shvedchikov <105097978+ConstableFraser@users.noreply.github.com>
2025-03-19 18:45:22 +0300

    Update CodingDecodingTest.java

--- src/test/java/org/shvedchikov/domidzebot/CodingDecodingTest.java
+++ src/test/java/org/shvedchikov/domidzebot/CodingDecodingTest.java
@@ -12,7 +12,8 @@ import static org.shvedchikov.domidzebot.component.CoderDecoder.decodeString;
 
 
 @SpringBootTest
-@TestPropertySource(properties = "appproperties.hash=djM7MT82MzYxgkA9iYtLdzeFSESCpzCuOptoS0mLMjKRT1dLRTEwUDBINk5rSzVFQDVFP4kzNjE9OTM2MTQzlUg2M001RT42SFE=")
+@TestPropertySource(
+    properties = "appproperties.hash=djM7MT82MzYxgkA9iYtLdzeFSESCpzCuOptoS0mLMjKRT1dLRTEwUDBINk5rSzVFQDVFP4kzNjE9OTM2MTQzlUg2M001RT42SFE=")
 public class CodingDecodingTest {
     @Autowired
     private CoderDecoder coderDecoder;
