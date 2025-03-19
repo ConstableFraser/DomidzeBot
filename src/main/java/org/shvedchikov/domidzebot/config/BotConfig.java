da0dc5d5881d09b165a2cd76e4ca6209e6b331a7
snswd <sanswed@gmail.com>
2025-03-19 20:49:47 +0300

    *fix bugs at props

--- src/main/java/org/shvedchikov/domidzebot/config/BotConfig.java
+++ src/main/java/org/shvedchikov/domidzebot/config/BotConfig.java
@@ -1,17 +1,17 @@
 package org.shvedchikov.domidzebot.config;
 
 import lombok.Data;
-// import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-// import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySource;
 
 @Data
 @Configuration
-// @PropertySource("classpath:application.properties")
+@PropertySource("classpath:application.properties")
 public class BotConfig {
-//    @Value(value = "${bot.name}")
+    @Value(value = "${bot.name}")
     private String name = "testnamebot";
 
-//    @Value(value = "${bot.token}")
+    @Value(value = "${bot.token}")
     private String token = "very secret API token";
 }
