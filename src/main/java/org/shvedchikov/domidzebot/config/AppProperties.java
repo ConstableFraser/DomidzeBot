75840081b8dc67473ecf4ad996c3f67ec476a372
snswd <sanswed@gmail.com>
2025-03-19 04:25:52 +0300

    *fix bugs at props

--- src/main/java/org/shvedchikov/domidzebot/config/AppProperties.java
+++ src/main/java/org/shvedchikov/domidzebot/config/AppProperties.java
@@ -3,9 +3,11 @@ package org.shvedchikov.domidzebot.config;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Data
 @Configuration
+@PropertySource("file:///src/main/test/resources/application-test.properties")
 public class AppProperties {
     @Value(value = "${appproperties.hash}")
     private String hash;
